c22295c22d279167aa60981abc01a6d8076f8e7d
sky <125726152+skyshourya@users.noreply.github.com>
2025-01-17 03:05:05 +0530

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -13,8 +13,8 @@ resource "aws_instance" "web" {
 }
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amiID.id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+  instance_type          = t2.micro
+  key_name               = jenag-key
   availability_zone      = var.aws_zone
   vpc_security_group_ids = [aws_security_group.yolo-sg.id]
 
