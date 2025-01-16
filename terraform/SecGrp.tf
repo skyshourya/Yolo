1ad23a26f483dce1e3e4b4047a399f8747f60bef
shourya yadav <sky@shouryas-MacBook-Air.local>
2025-01-17 01:50:31 +0530

    security group jenkins port update

--- terraform/SecGrp.tf
+++ terraform/SecGrp.tf
@@ -30,15 +30,22 @@ resource "aws_vpc_security_group_ingress_rule" "yolodockerport" {
   ip_protocol       = "tcp"
   to_port           = 8080
 }
+resource "aws_vpc_security_group_ingress_rule" "yolojenkinsportp" {
+  security_group_id = aws_security_group.yolo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8000
+  ip_protocol       = "tcp"
+  to_port           = 8000
+}
 
 resource "aws_vpc_security_group_egress_rule" "allowalloutbound_ipv4" {
   security_group_id = aws_security_group.yolo-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" 
 }
 
 resource "aws_vpc_security_group_egress_rule" "allowalloutbound_ipv6" {
   security_group_id = aws_security_group.yolo-sg.id
   cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" 
 }
